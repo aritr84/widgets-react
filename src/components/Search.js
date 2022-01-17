a483909a4d8f2124e9e59e9d48b9eb501f7a4013
aritr84 <65041325+aritr84@users.noreply.github.com>
2022-01-17 23:44:06 +0530

    Search component completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,32 +2,44 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = function () {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
 
   //   console.log(results);
+
   useEffect(() => {
-    // const search = async function () {
-    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-    //     params: {
-    //       action: "query",
-    //       list: "search",
-    //       origin: "*",
-    //       format: "json",
-    //       srsearch: term,
-    //     },
-    //   });
+    const search = async function () {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
 
-    //   setResults(data.query.search);
-    // };
+      setResults(data.query.search);
+    };
 
-    // const timeoutId = setTimeout(() => {
-    //   if (term) {
-    //     search();
-    //   }
-    // }, 500);
+    // if term is present and result state is empty
+    // it means --> this is the first time search
+    if (term && !results.length) {
+      search();
+    }
+    // else do use the timeout
+    else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
 
-    console.log("Initial render ot term was changed");
+      // clean up function -
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
@@ -104,5 +116,40 @@ search();
         <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
 
     
-    
-    */
+ß
+# How to clear a setTimeout()
+    - set timeout --> returns an identifier
+    - use clearTimeout(identifier) => to clear out the timer
+
+
+
+# previous version of use effect function -
+     - Here the code is making request as soon as the term changes
+     - This makes a lot of api get request
+     - to reduce the number of requests we are changing it to the above version of code  
+    useEffect(() => {
+      const search = async function () {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: term,
+          },
+        });
+
+        setResults(data.query.search);
+      };
+
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      console.log("Initial render of term was changed");
+
+
+    }, [term]);`
+*/
