ac41626e0c3dc6fe025424241b07e598c73ce9f2
aritr84 <65041325+aritr84@users.noreply.github.com>
2022-01-18 06:15:16 +0530

    Dropdown Started

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React from "react";
 //import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-// creating items array -
+// creating items array for Accordion -
 // then passing the items as a prop to Accordion component
-// const items = [
-//   {
-//     title: "What is react?",
-//     content: "React is front end javascript framework.",
-//   },
-//   {
-//     title: "Why use react?",
-//     content: "React is a favorite JS library among engineers.",
-//   },
-//   {
-//     title: "How do you use react",
-//     content: "You use react by creating components",
-//   },
-// ];
+const items = [
+  {
+    title: "What is react?",
+    content: "React is front end javascript framework.",
+  },
+  {
+    title: "Why use react?",
+    content: "React is a favorite JS library among engineers.",
+  },
+  {
+    title: "How do you use react",
+    content: "You use react by creating components",
+  },
+];
+
+// creating options array for Dropdown -
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of blue",
+    value: "blue",
+  },
+];
 
 const App = function () {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dropdown = function () {
+  return <div>Dropdown</div>;
+};
+
+export default Dropdown;
