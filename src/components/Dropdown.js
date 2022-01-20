2391d9e124daf964c00fc858f032e6c458bd94e1
aritr84 <65041325+aritr84@users.noreply.github.com>
2022-01-20 23:02:00 +0530

    addeventlistner

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = function ({ options, selected, onSelectedChange }) {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        console.log("Body clicked");
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
   const renderedOPtion = options.map((option) => {
+    // remove the duplicate element -
+    if (option.value === selected.value) {
+      return null;
+    }
     return (
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          console.log("Item Clicked");
+          onSelectedChange(option);
+        }}
       >
         {option.label}
       </div>
@@ -16,10 +34,21 @@ const Dropdown = function ({ options, selected, onSelectedChange }) {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            console.log("Dropdown clicked");
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible open" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOPtion}</div>
+          <div
+            onClick={() => setOpen(!open)}
+            className={`menu ${open ? `visible transition` : ""}`}
+          >
+            {renderedOPtion}
+          </div>
         </div>
       </div>
     </div>
